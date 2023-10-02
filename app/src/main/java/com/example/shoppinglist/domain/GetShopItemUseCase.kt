6172a3d65de317b0de6b6ddb6f8d3552b81632b8
Delya <delina.cat@gmail.com>
2023-10-02 18:41:31 +0300

    Domain layer

---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
@@ -0,0 +1,5 @@
+package com.example.shoppinglist.domain
+
+class GetShopItemUseCase (private val shopListRepository: ShopListRepository) {
+    fun getShopItem(shopItemId: Int): ShopItem = shopListRepository.getShopItem(shopItemId)
+}
---
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopListUseCase.kt
@@ -0,0 +1,5 @@
+package com.example.shoppinglist.domain
+
+class GetShopListUseCase (private val shopListRepository: ShopListRepository) {
+    fun getShopList(): List<ShopItem> = shopListRepository.getShopList()
+}
